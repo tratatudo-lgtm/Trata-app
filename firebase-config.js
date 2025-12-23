8f709709e16b506c3231f4b7518b395f9119a7d2
tratatudo-lgtm <u1749243275@gmail.com>
2025-12-23 16:57:53 +0000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Substitui os valores abaixo pelos dados reais do teu projeto Firebase
+const firebaseConfig = {
+  apiKey: "A_TUA_API_KEY",
+  authDomain: "O_TEU_DOMINIO.firebaseapp.com",
+  projectId: "O_TEU_PROJECT_ID",
+  storageBucket: "O_TEU_BUCKET.appspot.com",
+  messagingSenderId: "O_TEU_SENDER_ID",
+  appId: "A_TUA_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
